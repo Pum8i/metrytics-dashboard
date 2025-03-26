017c8ad1677e203b89f63f5809740dae3288f4d3
andy <apumbien@gmail.com>
2025-03-26 15:27:21 -0700

    Refactor analytics API to exclude browser version from visitor data extraction

--- app/api/analytics/route.ts
+++ app/api/analytics/route.ts
@@ -61,7 +61,7 @@ export async function POST(request: NextRequest) {
     // }
 
     const { city, country, location } = await getLocationInfo(ip ?? "");
-    const { browserName, browserVersion, osName } = getBrowserInfo(
+    const { browserName, osName } = getBrowserInfo(
       requestHeaders.get("userAgent") ?? ""
     );
 
