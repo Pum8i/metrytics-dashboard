06588a75706522fda1a9c46ab0f08ea4c3dbd895
andy <apumbien@gmail.com>
2025-04-08 12:11:13 -0700

    Refactor PageViews to use ViewsChart component; replace TopChart and add new ViewsChart implementation

--- app/dashboard/tabs/views/page.tsx
+++ app/dashboard/tabs/views/page.tsx
@@ -1,18 +1,18 @@
 import { IAnalyticsSummary } from "@/app/types";
 import React from "react";
-import TopChart from "./top-chart";
-import Tops from "./tops";
+import PieChart from "./pie-chart";
+import RawChart from "./raw-chart";
 
 export default function PageViews({ summary }: { summary: IAnalyticsSummary }) {
   return (
     <div className="flex flex-col md:flex-row w-full gap-4 h-full ">
       <div className="flex flex-col md:w-2/3 gap-4">
-        <Tops title="Top Referrers" tops={summary.topReferrers} />
-        <Tops title="Top Pages" tops={summary.topPages} />
+        <RawChart title="Top Referrers" data={summary.topReferrers} />
+        <RawChart title="Top Pages" data={summary.topPages} />
       </div>
       <div className="flex flex-col md:w-1/3 h-full gap-4">
-        <TopChart data={summary.countries.slice(0, 5)} label="Countries" />
-        <TopChart data={summary.cities.slice(0, 5)} label="Cities" />
+        <PieChart data={summary.countries.slice(0, 5)} label="Countries" />
+        <PieChart data={summary.cities.slice(0, 5)} label="Cities" />
         {/* <Tops title="Top Countries" tops={summary.countries} /> */}
         {/* <Tops title="Top Cities" tops={summary.cities} /> */}
       </div>
